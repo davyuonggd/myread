dfc83cda32c5cb3e7fab2a2994f4c6248503232a
Davy Uong <davyuonggd@gmail.com>
2018-02-11 13:21:48 -0800

    display found books on search

--- src/CreateBook.js
+++ src/CreateBook.js
@@ -7,33 +7,33 @@ import * as BooksAPI from './BooksAPI'
 class CreateBook extends Component {
 
   state = {
-    query: ''
+    foundBooks: []
+  }
+
+  clearFoundBooks = () => {
+    this.setState({foundBooks: []})
   }
 
   searchBooksWithSearchTerm = (searchTerm) => {
-    console.log(searchTerm)
-    BooksAPI.search(searchTerm).then((foundBooks)=>{
-      console.log(foundBooks)
+    console.log('searchTerm: ', searchTerm)
+    if (!searchTerm) {
+      this.clearFoundBooks()
+      return
+    }
+    BooksAPI.search(searchTerm).then((response) => {
+      console.log('response from search: ', response)
+      if (response.error) {
+        console.log('error: ', response.error)
+        this.clearFoundBooks()
+        return
+      }
+      this.setState({foundBooks: response})
     })
   }
 
-  updateQuery = (query) => {
-    this.setState({ query: query.trim() })
-  }
-
-  clearQuery = () => {
-    this.setState({ query: '' })
-  }
-
   render() {
-    const { query } = this.state
-
-    let foundBooks
-
-    if (query) {
-      this.searchBooksWithSearchTerm(query)
-    }
-
+    const { foundBooks } = this.state
+    console.log('foundBooks to render: ', foundBooks)
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -50,14 +50,41 @@ class CreateBook extends Component {
             <input
               type="text"
               placeholder="Search by title or author"
-              value={query}
-              onChange={(event)=>this.updateQuery(event.target.value)}
+              onChange={(event)=>this.searchBooksWithSearchTerm(event.target.value)}
               />
-
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {
+              (foundBooks.length !== 0) && (
+                foundBooks.map( (bookDict) => (
+                  <li key={bookDict.id}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div
+                          className="book-cover"
+                          style={{ width: 128, height: 193, backgroundImage: `url(${ bookDict.imageLinks.thumbnail })` }}>
+                        </div>
+                        <div className="book-shelf-changer">
+                          <select value='none'>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="book-title">{ bookDict.title }</div>
+                      <div className="book-authors">
+                      </div>
+                    </div>
+                  </li>
+                ))
+              )
+            }
+          </ol>
         </div>
       </div>
     )
